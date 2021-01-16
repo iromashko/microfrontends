d49de05733207e5a8d7366cd3ac1d969a5261863
iromashko <iromashko@me.com>
2021-01-16 12:13:17 +0300

    refactor

--- container/src/bootstrap.js
+++
@@ -1,8 +0,0 @@
-import { mount as productsMount } from 'products/ProductsIndex';
-import { mount as cartMount } from 'cart/CartShow';
-import 'cart/CartShow';
-
-console.log(`Container`);
-
-productsMount(document.querySelector('#dev-products'));
-cartMount(document.querySelector('#dev-cart'));
