d3159af3c844cef613dada9ad297ff4c5f6f2ebe
iromashko <iromashko@me.com>
2021-01-16 12:01:20 +0300

    part 4

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,8 @@
-import 'products/ProductsIndex';
+import { mount as productsMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 import 'cart/CartShow';
 
 console.log(`Container`);
+
+productsMount(document.querySelector('#dev-products'));
+cartMount(document.querySelector('#dev-cart'));
